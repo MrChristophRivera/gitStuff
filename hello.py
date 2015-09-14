958fff425c0161aa018a9601c768719d47451673
MrChristophRivera <chris.richard.rivera@gmail.com>
2015-09-14 13:58:49 -0700

    Created the hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print 'hello world'
+def hello_world(n=1):
+    '''prints hello world'''
+    for i in range(n): 
+        print 'hello World'
+
+
+if __name__ == '__main__':
+    hello_world(n=2)
+
+
